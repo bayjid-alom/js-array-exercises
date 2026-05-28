8c5d6493d8927960737ac132f1a1ba92283a8365
bayjidalom <bayjidalom1005@gmail.com>
2026-05-28 23:31:04 +0600

    traversal without reverse method

--- 16.5_summary.js
+++ 16.5_summary.js
@@ -1,7 +1,10 @@
 console.log("===========| Array traversal using for/while loop |==========")
 
 /*
- Looping technique:
+🔁 Array Traversal
+Traversal মানে array এর সব item এক এক করে দেখা।
+
+Looping technique's:
  1. for loop
  2. while loop
  3. do while loop
@@ -37,6 +40,49 @@ while (i < friends.length) {
     console.log(friends[i])
     i++;
 }
+
+
+
+
+console.log("===========| traversal without reverse method |==========")
+
+
+const numbers_2 = [11, 22, 33, 44, 55, 66];
+// const rev = numbers_2.reverse()
+// console.log("Using reverse method : ", rev)
+
+
+// Way -- 01 (without using reverse method)
+const reverse_numbers = [];
+for (const num of numbers_2) {
+    // console.log(num)
+    reverse_numbers.unshift(num)
+}
+console.log("Using unshift() :", reverse_numbers)
+
+
+
+// Way -- 02 (without using reverse method)
+const reversed = [];
+for (let i = 0; i < numbers_2.length; i++) {
+    // console.log(numbers_2[i])
+    reversed.unshift(numbers_2[i])
+}
+console.log("Using for loop: ", reversed)
+
+
+
+// Way -- 03 (without using reverse method)
+// reverse way loop
+
+const reverse_way = [];
+for (let i = numbers_2.length - 1; i >= 0; i--) {
+    // console.log(numbers_2[i])
+    reverse_way.push(i)
+}
+console.log("Using reverse way :", reversed)
+
+
 
 
 
